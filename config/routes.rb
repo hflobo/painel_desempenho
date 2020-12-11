8a37ea68e3998d4b04921ecb8f879264e469138d
Joao Velloso <joao.velloso@rfb.gov.br>
2020-12-11 19:49:01 -0300

    primeira versão do alerts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'alerts/index'
+  get 'alerts/show'
+  get 'alerts/new'
+  get 'alerts/create'
+  get 'alerts/edit'
+  get 'alerts/update'
+  get 'alerts/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
