2515d4cdc00f86488d10595f30694509ce1c401d
Joao Velloso <joao.velloso@rfb.gov.br>
2020-12-15 15:12:20 -0300

    menu dashboards

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_12_14_205245) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,15 +34,6 @@ ActiveRecord::Schema.define(version: 2020_12_14_205245) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "affiliateds", force: :cascade do |t|
-    t.bigint "indicator_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["indicator_id"], name: "index_affiliateds_on_indicator_id"
-    t.index ["user_id"], name: "index_affiliateds_on_user_id"
   end
 
   create_table "alerts", force: :cascade do |t|
@@ -126,15 +116,6 @@ ActiveRecord::Schema.define(version: 2020_12_14_205245) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "responsibles", force: :cascade do |t|
-    t.bigint "indicator_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["indicator_id"], name: "index_responsibles_on_indicator_id"
-    t.index ["user_id"], name: "index_responsibles_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -160,8 +141,6 @@ ActiveRecord::Schema.define(version: 2020_12_14_205245) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "affiliateds", "indicators"
-  add_foreign_key "affiliateds", "users"
   add_foreign_key "alerts", "indicators"
   add_foreign_key "alerts", "users", column: "criador_id"
   add_foreign_key "alerts", "users", column: "destinatario_id"
@@ -173,7 +152,5 @@ ActiveRecord::Schema.define(version: 2020_12_14_205245) do
   add_foreign_key "indicators", "users"
   add_foreign_key "kpis", "dashboards"
   add_foreign_key "kpis", "indicators"
-  add_foreign_key "responsibles", "indicators"
-  add_foreign_key "responsibles", "users"
   add_foreign_key "values", "indicators"
 end
