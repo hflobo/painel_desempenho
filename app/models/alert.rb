cb689d6ca41e7860f8f402d7510948b67659e8c8
Joao Velloso <joao.velloso@rfb.gov.br>
2020-12-15 11:27:37 -0300

    melhoramentos e correções de alerts, indicators e uso de includes

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -3,3 +3,9 @@ class Alert < ApplicationRecord
   belongs_to :criador, class_name: 'User'
   belongs_to :destinatario, class_name: 'User'
 end
+
+def name_of_method
+  name = "#{nome} - #{abrangencia}"
+  name = name + ": " + Region.find(region_id).nome if abrangencia.upcase! == "REGIONAL"
+  return name
+end
