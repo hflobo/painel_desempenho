d4f404710a1cb8491040ecd0d55177f421a89791
Hugo Lobo <hflobo@gmail.com>
2020-12-16 12:16:11 -0300

    versão inicial do dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'dashboards#show'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
   get 'alerts_sent', to: 'alerts#alerts_sent'
   get 'alerts_received', to: 'alerts#alerts_received'
   get 'alerts_filed', to: 'alerts#alerts_filed'
+
+  resources :dashboards, only: %i[show new create destroy] do
+    resources :kpis, only: %i[show new create destroy]
+  end
 end
