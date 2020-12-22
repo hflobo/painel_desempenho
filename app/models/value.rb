16e546f9a4c3862d361204ea82eff1c41f3a86d9
Joao Velloso <joao.velloso@rfb.gov.br>
2020-12-22 05:28:57 -0300

    correções diversas

--- app/models/value.rb
+++ app/models/value.rb
@@ -3,7 +3,7 @@ class Value < ApplicationRecord
 
   validates :indicator_id, :ano, :periodo, :valor, presence: true
   validates :periodo, uniqueness: { scope: %i[ano indicator_id],
-                                    message: "Período deve ser único dentro de um ano para um indicador" }
+                                    message: "deve ser único dentro de um ano para um indicador" }
   validates :periodo, numericality: { less_than_or_equal_to: ->(value) { value.indicator.qtd_apuracoes_ano },
                                       message: "Um indicador não pode ter mais valores em um ano que o indicado em seu cadastro." }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # root to: 'dashboards#show'
-
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :indicators do
